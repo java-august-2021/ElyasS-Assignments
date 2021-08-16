bb66ec6acb0b24159071e41949896e93c4540a10
elyassepasi <81493531+elyassepasi@users.noreply.github.com>
2021-08-15 18:40:41 -0700

    Add files via upload

---
+++ 02-java-oop/06-object-master-p2/HumanTest.java
@@ -0,0 +1,8 @@
+public class HumanTest {
+    public static void main(String[] args) {
+        Human attackerMan = new Human();
+        Human defenceMan = new Human();
+
+        attackerMan.attack(defenceMan);
+    }
+}
